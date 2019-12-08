86e2e2c6f0049c62d0e1a4e4d581fdd45c5845f1
tkyk.niimura <tkyk.niimura@gmail.com>
2019-12-08 23:21:51 +0900

    プロジェクト ファイルを追加します。

---
+++ SampleRazorApp/Pages/Form.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace SampleRazorApp.Pages
+{
+    public class FormModel : PageModel
+    {
+
+        public string  Message { get; set; }
+        
+        [DataType(DataType.Text)]
+        public string  Name { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string Mail { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        public string Tel { get; set; }
+
+        public void OnGet()
+        {
+            Message = "何か入力してください。";
+        }
+
+        public void OnPost(string name, string password, string mail, string tel)
+        {
+            Message = $"[name:{name}, password:{password}, mail:{mail}, tel:{tel}]";
+        }
+    }
+}
---
+++ SampleRazorApp/Pages/Index.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace SampleRazorApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        [ViewData]
+        public string Message { get; set; }
+        private string _name = "no-name";
+        private string _mail = "no-mail";
+        private string[][] data = { 
+            new[] { "taro", "taro@yamada" }, 
+            new[] { "hanako", "hanako@flower" }, 
+            new[] { "sachiko", "sachiko@happy" } };
+
+        [BindProperty(SupportsGet = true)]
+        public int id { get; set; }
+
+        public string getData(int id)
+        {
+            return $"[名前:{data[id][0]} ,メール:{data[id][1]}]";
+        }
+
+        public void OnGet()
+        {
+            //this.ViewData["message"] = "This is sample message";
+            this.Message = "何か入力して下さい";
+        }
+
+        public string getData()
+        {
+            return $"[名前:{_name} ,メール:{_mail}]";
+        }
+    }
+}
